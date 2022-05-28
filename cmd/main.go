b84b0179cf2c18a804841abebae5894748a365ee
zikster <zikster3262@gmail.com>
2022-05-28 15:47:07 +0200

    first commits

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	service "github.com/zikster3262/go-grpc-order-svc/pkg/services"
+	"github.com/ziskter3262/go-grpc-order-svc/pkg/client"
+	"github.com/ziskter3262/go-grpc-order-svc/pkg/config"
+	"github.com/ziskter3262/go-grpc-order-svc/pkg/db"
+	"github.com/ziskter3262/go-grpc-order-svc/pkg/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	productSvc := client.InitProductServiceClient(c.ProductSvcUrl)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Order Svc on", c.Port)
+
+	s := service.Server{
+		H:          h,
+		ProductSvc: productSvc,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterOrderServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
